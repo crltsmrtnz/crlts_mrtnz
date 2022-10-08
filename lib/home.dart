170802d3b62c2dbbd14cdfde10328a8d2e7aefc3
crltsmrtnz <crltsmrtnz@gmail.com>
2022-10-08 12:35:49 -0300

    home base

---
+++ lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:porfolio/home.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("hola"),
+    );
+  }
+}
