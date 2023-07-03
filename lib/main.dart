cb4b85ebe1e772adce62bfe174378ccb8892f8bd
crltsmrtnz <crltsmrtnz@gmail.com>
2023-07-03 19:07:30 -0400

    dev for action test

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Crlts Mrtnz',
+      title: 'CrltsMrtnz',
       theme: ThemeData(
         textTheme: GoogleFonts.robotoMonoTextTheme(
           Theme.of(context).textTheme,
