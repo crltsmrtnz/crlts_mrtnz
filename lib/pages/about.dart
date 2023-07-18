fbe89684a3e3d32876556b2f6aaa1ff84b09bd80
Carlos Martinez <crltsmrtnz@MacBook-Pro-de-Carlos.local>
2023-07-18 13:42:36 -0400

    migrate to crltsmrtnx

---
+++ lib/pages/about.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import 'package:crltsmrtnz/pages/nav_bar.dart';
+
+class About extends StatelessWidget {
+  const About({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        drawer: const NavBar(),
+        appBar: AppBar(
+          title: const Text("About me"),
+        ),
+        body: const Center(
+          // Este Widget de texto usará la fuente RobotoMono
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 52,
+                backgroundImage: AssetImage('assets/img/CrltsMrtnz.png'),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                "CrltsMrtnz@gmail.com",
+                style: TextStyle(fontSize: 24, color: Color(0xFF1434D7)),
+              ),
+              Text(
+                "+595972264992",
+                style: TextStyle(fontSize: 24, color: Color(0xFF1434D7)),
+              ),
+              Text(
+                "https://www.linkedin.com/in/crltsmrtnz/",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, color: Color(0xFF1434D7)),
+              )
+            ],
+          ),
+        ),
+      );
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:crltsmrtnz/pages/nav_bar.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        drawer: const NavBar(),
+        appBar: AppBar(
+          title: const Text("Home"),
+        ),
+        body: const SafeArea(
+          child: Center(
+            // Este Widget de texto usará la fuente RobotoMono
+            child: Text(
+              'OK firebase conect. Contact channel in readme',
+            ),
+          ),
+        ),
+      );
+}
